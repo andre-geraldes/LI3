813033ae8199c818a510a01d9cbc4dcb8a6f0f20
BBarbosa <brunobarbosa.caos@gmail.com>
2015-04-21 23:44:42 +0100

    Added menus

---
+++ menus.h
@@ -0,0 +1,14 @@
+#ifndef MENUS_H
+#define MENUS_H
+
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+int menuOpcoes();
+
+void executaQuery();
+
+void carregaTextArt(char *nome_ficheiro);
+
+#endif
